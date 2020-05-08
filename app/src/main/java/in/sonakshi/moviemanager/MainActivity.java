4a25c91d62b75b320f0e00d52205d695a1721fb0
Sonakshi Priya <sonakshipriya2017@gmail.com>
2020-05-08 20:27:35 +0530

    Added RecyclerView to show list of movies.

--- app/src/main/java/in/sonakshi/moviemanager/MainActivity.java
+++ app/src/main/java/in/sonakshi/moviemanager/MainActivity.java
@@ -2,12 +2,45 @@ package in.sonakshi.moviemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.sonakshi.moviemanager.adapter.MovieAdapter;
+import in.sonakshi.moviemanager.models.Movie;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_movie);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // 1. Layout Manager
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<Movie> movies = new ArrayList< >();
+        List < String > gs = new ArrayList <>();
+        gs.add ("Thriller"); gs.add ("Comedy"); gs.add ("SciFi"); gs.add ("Romantic");
+        for (int i = 0; i < 20; i++) {
+            movies.add (new Movie (i, "Title" + i, "image" + i, 5 + i, 2010 + i, gs));
+        }
+
+        // 2. Adapter
+        mAdapter = new MovieAdapter(movies);
+        recyclerView.setAdapter(mAdapter);
+
     }
 }
---
+++ app/src/main/java/in/sonakshi/moviemanager/adapter/MovieAdapter.java
@@ -0,0 +1,96 @@
+package in.sonakshi.moviemanager.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import in.sonakshi.moviemanager.R;
+import in.sonakshi.moviemanager.models.Movie;
+
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private List < Movie > mMovies;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        private ImageView movieImageView;
+        private TextView nameTextView;
+        private TextView ratingTextView;
+        private TextView genreTextView;
+        private TextView releaseYearTextView;
+
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+
+            movieImageView = (ImageView) itemView.findViewById(R.id.image_view_movie);
+            nameTextView = (TextView) itemView.findViewById(R.id.text_view_movie_name);
+            ratingTextView = (TextView) itemView.findViewById(R.id.text_view_movie_rating);
+            genreTextView = (TextView) itemView.findViewById(R.id.text_view_movie_genre);
+            releaseYearTextView = (TextView) itemView.findViewById(R.id.text_view_movie_releaseYear);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MovieAdapter(List< Movie > movies) {
+        mMovies = movies;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MovieAdapter.MovieViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View movieView = inflater.inflate(R.layout.movie_items, parent, false);
+
+        // Return a new holder instance
+        return new MovieViewHolder(movieView);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        Movie movie = mMovies.get(position);
+
+        TextView nameTextView = holder.nameTextView;
+        nameTextView.setText(movie.getTitle());
+        TextView ratingTextView = holder.ratingTextView;
+        ratingTextView.setText("Rating: " + String.valueOf(movie.getRating()));
+        TextView genreTextView = holder.genreTextView;
+        StringBuilder genreStringBuilder = new StringBuilder();
+        int index = 0;
+        for (String genre : movie.getGenre()) {
+            if (index > 0) {
+                genreStringBuilder.append (", ");
+            }
+            genreStringBuilder.append(genre);
+            index++;
+        }
+        genreTextView.setText(genreStringBuilder.toString());
+        TextView releaseYearTextView = holder.releaseYearTextView;
+        releaseYearTextView.setText(String.valueOf(movie.getReleaseYear()));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mMovies.size();
+    }
+}
